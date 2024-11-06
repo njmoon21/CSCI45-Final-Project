8bb67899dd5cf114e1ce7fd0da92d37d26b7aa75
SunnyFGC <163361000+njmoon21@users.noreply.github.com>
2024-11-05 23:08:09 -0800

    Platforms
    Added platforms for testing vertical camera offset

--- Character/player.gd
+++ Character/player.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 
 @export var speed: float = 165.0
 @export var jump_velocity: float = -200.0
-@export var double_jump_velocity: float = -170.0
+@export var double_jump_velocity: float = -180.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
