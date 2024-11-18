93262d81976680f4374fcc04dba4b61a1c62560a
SunnyFGC <163361000+njmoon21@users.noreply.github.com>
2024-11-18 08:33:37 -0800

    LOTS OF CHANGES (PLEASE READ)
    - Created another jump function when bouncing on enemies (replaces jump sfx with dying sfx)
    
    - Added functionality to volume sliders and full screen button in the settings menu
    
    - Added scrolling backgrounds in the menu + settings screen
    
    - Added an autoload file for menu music (created to make the music play between the two scenes without being interrupted until you hit PLAY)
    - Also created so that when you change the volume slider, it will store that saved volume value in the global variable. When you open the settings menu once more, it will load the previously saved value.
    
    - Changed the music (OMORI ---> Hotel Dusk: Room 215) + Added looping to the music

--- Character/Enemies/Pig/pig_enemy.gd
+++ Character/Enemies/Pig/pig_enemy.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
 @onready var animated_sprite : AnimatedSprite2D = $AnimatedSprite2D
+@onready var die_audio = $dieSFX
 
 func _ready():
 	add_to_group("Enemy")
@@ -13,7 +14,9 @@ func _on_hitbox_body_entered(body):
 		var y_delta = position.y - body.position.y
 		if (y_delta > 20):
 			print("Destroy Enemy")
-			body.jump()
+			body.enemy_bounce()
+			die_audio.play()
+			
 			animated_sprite.play("die")
 			await animated_sprite.animation_finished
 			queue_free()
--- Scenes/Menus/Main Menu/main_menu.gd
+++ Scenes/Menus/Main Menu/main_menu.gd
@@ -1,15 +1,18 @@
 extends Control
 
 @onready var tap_audio = $TapSFX
+@onready var start_button = $startButton
 
 ## Called when the node enters the scene tree for the first time.
-#func _ready() -> void:
-	#pass # Replace with function body.
-#
-#
+func _ready():
+	MenuMusic.play_menu_music()
+	start_button.grab_focus()
+
+
 ## Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta: float) -> void:
 	#pass
+
 
 # Show the name of the Game
 func show_title(text):
@@ -18,6 +21,7 @@ func show_title(text):
 
 # Pressing Play
 func _on_start_button_pressed():
+	MenuMusic.stop()
 	tap_audio.play()
 	
 	Transitioner.transition()
@@ -42,3 +46,11 @@ func _on_quit_button_pressed():
 	await Transitioner.on_transition_finished
 	
 	get_tree().quit()
+
+# Plays audio when navigating through all the buttons in the menu
+func _on_start_button_focus_entered():
+	tap_audio.play()
+func _on_settings_button_focus_entered():
+	tap_audio.play()
+func _on_quit_button_focus_entered():
+	tap_audio.play()
