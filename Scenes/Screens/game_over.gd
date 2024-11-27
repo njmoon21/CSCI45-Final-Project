b4e4cbf9857f189b35a0210cffcad00f434b88d7
Nathan <njluna21@yahoo.com>
2024-11-26 23:04:46 -0800

    Fixed Checkpoint bug after a game over / new run

--- Scenes/Screens/game_over.gd
+++ Scenes/Screens/game_over.gd
@@ -12,8 +12,15 @@ func _input(event: InputEvent):
 	if event is InputEventKey and event.pressed:
 		$GameOverMusic.stop()
 		$TapSFX.play()
+		
 		Transitioner.transition()
 		await Transitioner.on_transition_finished
+		
+		CheckpointManagement.activated = false
+		CheckpointManagement.last_position = null
+		
 		Globals.player_lives = 3
+		
 		MenuMusic.play()
+		
 		get_tree().change_scene_to_file("res://Scenes/Menus/Main Menu/main_menu.tscn")
