3ad8117cf996435698c16d830b6de8464d95efbf
Nathan <njluna21@yahoo.com>
2024-11-27 21:30:36 -0800

    Fixed Quick Game Over Bug

--- Character/Enemies/SpikeHead/thwomp.gd
+++ Character/Enemies/SpikeHead/thwomp.gd
@@ -48,9 +48,9 @@ func _on_timer_timeout() -> void:
 		print("Reloading Scene")
 		get_tree().reload_current_scene() #restarts level
 	else:
-		pass
+		print("Game Over")
 		queue_free()
-		#get_tree().change_scene_to_file("res://Scenes/Screens/game_over.tscn")
+		get_tree().change_scene_to_file("res://Scenes/Screens/game_over.tscn")
 
 
 func fall():
